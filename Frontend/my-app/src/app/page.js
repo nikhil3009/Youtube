c8ce88356105886c9d8f5d553f1900319e77ecfb
Sai Nikhil Reddy <sainikhil3009@gmail.com>
2024-11-10 03:29:41 -0500

    Youtube Vedio upload to AWS S3-bucket

---
+++ Frontend/my-app/src/app/page.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import Room from './pages/Room';
+import UploadForm from './components/UploadForm';
+
+export default function Home() {
+	return (
+		<>
+			<UploadForm></UploadForm>
+			<Room></Room>
+		</>
+	);
+}
